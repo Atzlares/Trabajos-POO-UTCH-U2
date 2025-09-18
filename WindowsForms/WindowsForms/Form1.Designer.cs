0f53e1f465dcedb8702be58870de5086e400811a
Atzlares <158379505+Atzlares@users.noreply.github.com>
2025-09-18 08:26:48 -0600

    Add files via upload

---
+++ WindowsForms/WindowsForms/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace WindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.button1.Location = new System.Drawing.Point(284, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Botón";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.AutoSize = true;
+            this.txt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt1.Location = new System.Drawing.Point(327, 122);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(103, 20);
+            this.txt1.TabIndex = 1;
+            this.txt1.Text = "Click al boton";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label txt1;
+    }
+}
+
---
+++ WindowsForms/WindowsForms/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public int color = 1;
+        public int fontSize = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txt1.Text = "¡Hola, Mundo!";
+            switch(color)
+            {
+                case 0:
+                    txt1.BackColor = Color.Coral;
+                    //txt1.Font = new Font(txt1.Font.FontFamily, 8);
+                    color++;
+                    break;
+                case 1:
+                    txt1.BackColor = Color.Aqua;
+                    //txt1.AutoSizeChanged += (s, ev) => { txt1.Font = new Font(txt1.Font.FontFamily, 12); };
+                    color++;
+                    break;
+                case 2:
+                    txt1.BackColor = Color.Crimson;
+                    color++;
+                    break;
+                case 3:
+                    txt1.BackColor = Color.DarkCyan;
+                    color++;
+                    break;
+                case 4:
+                    txt1.BackColor = Color.Gold;
+                    color++;
+                    break;
+                default:
+                    txt1.BackColor = Color.BlueViolet;
+                    color++;
+                    break;
+            }
+
+        }
+    }
+}
