3c7307d082d3df5b3e804b7e34a0b49d169d717b
Atzlares <158379505+Atzlares@users.noreply.github.com>
2025-09-19 12:06:02 -0600

    Add files via upload

---
+++ Login/Login/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string userC = "Axel";
+            string contraC = "atzl";
+            
+            if (textBox1.Text == userC && textBox2.Text == contraC)
+            {
+                MessageBox.Show("Inicio de sesión exitoso");
+                this.Hide();
+                Form1 form1 = new Form1();
+                form1.Show();
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrectos");
+            }
+        }
+    }
+}
